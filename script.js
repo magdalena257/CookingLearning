d708ac769645fd3896a994eaf3f3f45d5ab7394b
magdalena257 <1magdalenamuzyk@gmail.com>
2018-09-24 12:53:57 +0200

    descriptionFeature problem solved

--- script.js
+++ script.js
@@ -16,40 +16,50 @@ function activeContent() {
     }
 }
 
-$('.shoplist').on('click', shoplistFunction);
+// $('.shoplist').on('click', shoplistFunction);
 
-function shoplistFunction() {
-    console.log('shoplistFunction');
-    $('.shoplisted').toggleClass('activeFeature');
-};
+// function shoplistFunction() {
+//     console.log('shoplistFunction');
+
+// };
 
 
 $('.feature').on('click', showDescription);
 
 function showDescription() {
     $('.feature').removeClass('largerFeature');
+    $('.shoplisted').removeClass('activeFeature');
     $('.feature').addClass('smallerFeature');
     $(this).removeClass('smallerFeature');
     $(this).addClass('largerFeature');
+    $('.descriptionFeature').css({
+        'font-size': '0',
+        'padding': 'none'
+    });
+    if ($(this).hasClass('largerFeature')) {
+        $('.feature').removeClass('largerFeature');
+        $('.descriptionFeature').css({
+            'font-size': '0',
+            'padding': 'none'
+        });
+        $('.feature').removeClass('smallerFeature');
 
+    }
     if ($(this).hasClass('shoplist')) {
         $('.shoplist .descriptionFeature').css({
             'font-size': '1.5rem',
             'padding': '5px'
         });
+        $('.shoplisted').addClass('activeFeature');
 
+    }
+    if ($(this).hasClass('scale')) {
+        $('.scale .descriptionFeature').css({
+            'font-size': '1.5rem',
+            'padding': '5px'
+        });
+        // $('.shoplisted').addClass('activeFeature');
 
     }
 
-
 }
-// switch(expression) {
-//     case x:
-//         code block
-//         break;
-//     case y:
-//         code block
-//         break;
-//     default:
-//         code block
-// }
